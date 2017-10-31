629c93cadb0974e9cc55b3fcb4a19321fdd1fcf8
lorenmh <neutronstar@gmail.com>
2017-10-30 17:52:55 -0700

    pushy push

--- p4/src/Process.java
+++ p4/src/Process.java
@@ -2,4 +2,20 @@
  * Created by loren on 10/30/17.
  */
 public class Process {
+    public static int counter = 0;
+
+    String id;
+    int numPages;
+    int totalRunTime;
+    int lastUsedPage; // for determining locality of reference
+
+    public Process(String id, int numPages, int totalRunTime) {
+        this.id = id;
+        this.numPages = numPages;
+        this.totalRunTime = totalRunTime;
+    }
+
+    public static Process ProcessFactory() {
+        // create a random process, id from counter
+    }
 }
