52a208488901024bbd4a511b6a58e580e63c6c50
yzhang <azraelexia@gmail.com>
2017-11-02 23:10:18 -0700

    LRU

---
+++ p4/src/LRU.java
@@ -0,0 +1,35 @@
+public class LRU implements ReplacementAlgorithm {
+    @Override
+    public int getNewFrame(Page[] pageArray, int processID, int pageID) {
+        boolean isNewProcess = true;
+        int leastRecentReferenceTime = Integer.MIN_VALUE;
+        int leastRecentReferenceIndex = -1;
+        int freePageCount = 0;
+        int firstFreePageIndex = -1;
+
+        for (int i = pageArray.length - 1; i >= 0; i--) {
+            if (pageArray[i].used) {
+                if (pageArray[i].processID == processID) {
+                    isNewProcess = false;
+                }
+                if (leastRecentReferenceTime > pageArray[i].lastReferenceTime) {
+                    leastRecentReferenceIndex = i;
+                    leastRecentReferenceTime = pageArray[i].lastReferenceTime;
+                }
+            }else {
+                freePageCount += 1;
+                firstFreePageIndex = i;
+            }
+        }
+
+        if (isNewProcess) {
+            if (freePageCount >= 4) {
+                return firstFreePageIndex;
+            }else {
+                return -1;
+            }
+        }else {
+            return leastRecentReferenceIndex;
+        }
+    }
+}
