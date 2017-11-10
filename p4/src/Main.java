06cced68f9d4a64d9518ab4aca3f7b07ba04e24b
lorenmh <neutronstar@gmail.com>
2017-11-10 12:49:58 -0800

    commit this shit

--- p4/src/Main.java
+++ p4/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scheduler scheduler = new Scheduler();
         Scheduler.printQueue(scheduler.arrivalQueue);
+
         PageTable table = new PageTable();
 
 
